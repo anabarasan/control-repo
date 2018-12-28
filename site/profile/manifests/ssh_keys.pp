ea239bb6e162e676a3f50e470bf8a2de4836e967
Shakir Shakiel <shakirshakiel@gmail.com>
2018-12-28 13:35:25 +0530

    Add exported resource

--- site/profile/manifests/ssh_keys.pp
+++ site/profile/manifests/ssh_keys.pp
@@ -19,4 +19,27 @@ class profile::ssh_keys (
     mode    => '644',
   }
 
+  $splits = split($public_key, " ")
+  @@ssh_authorized_key { $splits[2]:
+    ensure => present,
+    type   => $splits[0],
+    key    => $splits[1],
+    user   => "root",
+    tag    => 'ssh-key'
+  }
+
+  user { 'root':
+    ensure         => present,
+    purge_ssh_keys => false,
+  }
+
+  @@sshkey { $::facts['networking']['fqdn']:
+    host_aliases => [$::facts['networking']['hostname'], $::facts['ec2_metadata']['public-ipv4']],
+    key          => $::facts['sshecdsakey'],
+    type         => 'ecdsa-sha2-nistp256',
+    tag          => 'host-key'
+  }
+
+  Sshkey<<| tag == 'host-key'|>>
+  Ssh_authorized_key <<| tag == 'ssh-key' |>>
 }
